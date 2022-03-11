0571893297aefedfb4fee3477b54bdd0716aecf8
jacob-tucker <jacobtucker818@gmail.com>
2022-03-10 22:38:14 -0600

    updated tests

--- cadence/tests/src/common.js
+++ cadence/tests/src/common.js
@@ -6,7 +6,9 @@ const UFIX64_PRECISION = 8;
 // UFix64 values shall be always passed as strings
 export const toUFix64 = (value) => value.toFixed(UFIX64_PRECISION);
 
-export const getNumberAdminAddress = async () => getAccountAddress("NumberAdmin");
+export const getNFTAdminAddress = async () => getAccountAddress("NFTAdmin");
+export const getTenantAddress = async () => getAccountAddress("Tenant");
+export const getUserAddress = async () => getAccountAddress("User");
 
 export const sendTransactionWithErrorRaised = async (...props) => {
   const [resp, err] = await sendTransaction(...props);
